ce88fddeeaca542234f561258bda248c42a75fa2
minhy <minhy@DESKTOP-9Q7G4D3>
2022-06-21 00:39:11 +0900

    change in master after branching dev

--- HelloGit/src/Myclass.java
+++ HelloGit/src/Myclass.java
@@ -3,5 +3,6 @@ public class Myclass {
 	public static void main(String args[]) {
 		System.out.println("Hello Git");
 		System.out.println("Hello I'm MH");
+		System.out.println("change in master branch");
 	}
 }
