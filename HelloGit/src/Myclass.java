f05a81e1cf57ea1690d12ce8fe3ac1b5724ba682
minhy <minhy@DESKTOP-9Q7G4D3>
2022-06-21 00:35:03 +0900

    Firsy commit in dev branch

--- HelloGit/src/Myclass.java
+++ HelloGit/src/Myclass.java
@@ -3,5 +3,9 @@ public class Myclass {
 	public static void main(String args[]) {
 		System.out.println("Hello Git");
 		System.out.println("Hello I'm MH");
+		MyHello myHello = new MyHello();
+		myHello.sayHello();
+	
 	}
+
 }
